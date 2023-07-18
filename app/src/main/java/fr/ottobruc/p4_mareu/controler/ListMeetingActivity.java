39b831d4b12632b92bed57fdba64ff2fa95f21ed
JeanOTTO <124897888+JeanOTTO@users.noreply.github.com>
2023-07-18 02:48:32 +0200

    Add feature Filter Meetings Create Menu Toolbar Layout : Create Menu Toolbar Filter Repository : Add methods Filter Controler : Edit ListMeetingActivity

--- app/src/main/java/fr/ottobruc/p4_mareu/controler/ListMeetingActivity.java
+++ app/src/main/java/fr/ottobruc/p4_mareu/controler/ListMeetingActivity.java
@@ -1,14 +1,26 @@
 package fr.ottobruc.p4_mareu.controler;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+
+import fr.ottobruc.p4_mareu.R;
 import fr.ottobruc.p4_mareu.databinding.ActivityListmeetingBinding;
 import fr.ottobruc.p4_mareu.di.DI;
 import fr.ottobruc.p4_mareu.model.Meeting;
+import fr.ottobruc.p4_mareu.model.Room;
 import fr.ottobruc.p4_mareu.repository.MeetingRepository;
 
 public class ListMeetingActivity extends AppCompatActivity implements MeetingAdapter.Listener {
@@ -47,6 +59,100 @@ public class ListMeetingActivity extends AppCompatActivity implements MeetingAda
         meetingRepository.deleteMeeting(meeting);
         Toast.makeText(this, "Réunion supprimée !", Toast.LENGTH_SHORT).show();
         initList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_filter_off) {
+            // Action de filtrage par date
+            initList();
+            return true;
+        }
+        if (id == R.id.action_filter_date) {
+            // Action de filtrage par date
+            showDatePickerDialog();
+            return true;
+
+        } else if (id == R.id.action_filter_room) {
+            // Action de filtrage par salle
+            showRoomFilterDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showDatePickerDialog() {
+        // Obtenez la date actuelle
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // Créez un sélecteur de date
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, selectedYear, selectedMonth, selectedDay) -> {
+            // Mettez à jour la date sélectionnée
+            Calendar selectedCalendar = Calendar.getInstance();
+            selectedCalendar.set(selectedYear, selectedMonth, selectedDay);
+            Date selectedDate = selectedCalendar.getTime();
+
+            // Filtrez les réunions en fonction de la date sélectionnée
+            List<Meeting> filteredMeetings = meetingRepository.getFilteredMeetingsByDate(selectedDate);
+
+            // Mettez à jour l'adaptateur de la liste des réunions avec les réunions filtrées
+            adapter.setMeetings(filteredMeetings);
+            adapter.notifyDataSetChanged();
+        }, year, month, day);
+
+        // Affichez le sélecteur de date
+        datePickerDialog.show();
+    }
+
+    private void showRoomFilterDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Filtrer par salle");
+
+        // Créez un tableau de noms de salle à partir de la liste des salles disponibles
+        String[] roomNames = new String[meetingRepository.getRooms().size()];
+        for (int i = 0; i < meetingRepository.getRooms().size(); i++) {
+            roomNames[i] = meetingRepository.getRooms().get(i).getName();
+        }
+
+        // Tableau pour stocker les salles sélectionnées
+        boolean[] checkedItems = new boolean[meetingRepository.getRooms().size()];
+
+        builder.setMultiChoiceItems(roomNames, checkedItems, (dialog, which, isChecked) -> checkedItems[which] = isChecked);
+
+        builder.setPositiveButton("Filtrer", (dialog, which) -> {
+            List<Meeting> filteredMeetings = filterMeetingsByRoom(checkedItems);
+
+            adapter.setMeetings(filteredMeetings);
+            adapter.notifyDataSetChanged();
+        });
+
+        builder.setNegativeButton("Annuler", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private List<Meeting> filterMeetingsByRoom(boolean[] checkedItems) {
+        List<Meeting> filteredMeetings;
+        filteredMeetings = new ArrayList<>();
+        for (int i = 0; i < checkedItems.length; i++) {
+            if (checkedItems[i]) {
+                Room room = meetingRepository.getRooms().get(i);
+                filteredMeetings.addAll(meetingRepository.getFilteredMeetingsByRoom(room));
+            }
+        }
+        return filteredMeetings;
     }
 
 }
--- app/src/main/java/fr/ottobruc/p4_mareu/repository/MeetingRepository.java
+++ app/src/main/java/fr/ottobruc/p4_mareu/repository/MeetingRepository.java
@@ -1,5 +1,9 @@
 package fr.ottobruc.p4_mareu.repository;
 
+import static fr.ottobruc.p4_mareu.utils.DateTimeUtil.isSameDate;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import fr.ottobruc.p4_mareu.model.Meeting;
 import fr.ottobruc.p4_mareu.model.Room;
@@ -31,6 +35,27 @@ public class MeetingRepository {
 
     public List<User> getUsers() {
         return apiService.getUsers();
+    }
+
+    public List<Meeting> getFilteredMeetingsByDate(Date selectedDate) {
+        List<Meeting> filteredMeetings = new ArrayList<>();
+
+        for (Meeting meeting : apiService.getMeetings()) {
+            if (isSameDate(meeting.getDate(), selectedDate)) {
+                filteredMeetings.add(meeting);
+            }
+        }
+        return filteredMeetings;
+    }
+
+    public List<Meeting> getFilteredMeetingsByRoom(Room room) {
+        List<Meeting> filteredMeetings = new ArrayList<>();
+        for (Meeting meeting : apiService.getMeetings()) {
+            if (meeting.getLocation().getName().equals(room.getName())) {
+                filteredMeetings.add(meeting);
+            }
+        }
+        return filteredMeetings;
     }
 
 }
