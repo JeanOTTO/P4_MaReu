a6821ac8f070ce9c945e29d4dd6b4e2a58d7cd0b
JeanOTTO <124897888+JeanOTTO@users.noreply.github.com>
2023-07-19 06:46:20 +0200

    Add Instrumented Test

--- app/src/androidTest/java/fr/ottobruc/p4_mareu/controler/AddMeetingActivityTest.java
+++ app/src/androidTest/java/fr/ottobruc/p4_mareu/controler/AddMeetingActivityTest.java
@@ -1,14 +1,9 @@
 package fr.ottobruc.p4_mareu.controler;
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.pressBack;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -20,15 +15,11 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static fr.ottobruc.p4_mareu.controler.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static fr.ottobruc.p4_mareu.service.DummyMeetingGenerator.DUMMY_MEETINGS;
-import static fr.ottobruc.p4_mareu.service.DummyMeetingGenerator.DUMMY_ROOMS;
-import static fr.ottobruc.p4_mareu.service.DummyMeetingGenerator.DUMMY_USERS;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import androidx.test.espresso.DataInteraction;
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -42,13 +33,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import java.text.ParseException;
-import java.util.Arrays;
+
 import fr.ottobruc.p4_mareu.R;
 import fr.ottobruc.p4_mareu.di.DI;
-import fr.ottobruc.p4_mareu.model.Meeting;
 
 @RunWith(AndroidJUnit4.class)
-public class CreateMeetingTest {
+public class AddMeetingActivityTest {
 
     @Rule
     public ActivityScenarioRule<ListMeetingActivity> mActivityScenarioRule =
